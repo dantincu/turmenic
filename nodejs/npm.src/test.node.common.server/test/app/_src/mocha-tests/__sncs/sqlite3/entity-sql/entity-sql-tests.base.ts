28eaec45837558f07fff4a7d55d0b3914f3e6035
Daniel Tincu <tincuvd@gmail.com>
2021-05-01 07:38:24 +0300

    Added mocha test for sqlite3 table creating, populating, retrieving and converting data both wats

--- nodejs/npm.src/test.node.common.server/test/app/_src/mocha-tests/__sncs/sqlite3/entity-sql/entity-sql-tests.base.ts
+++ nodejs/npm.src/test.node.common.server/test/app/_src/mocha-tests/__sncs/sqlite3/entity-sql/entity-sql-tests.base.ts
@@ -28,3 +28,25 @@ export const sampleNegativeIntNumbers = sampleIntNumbers.map((n) => -n);
 
 export const sampleFloatNumbers = [0.01, 0.037, 0.99, 1.01, 1.23, 123.456];
 export const sampleNegativeFloatNumbers = sampleFloatNumbers.map((n) => -n);
+
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  discount: number;
+}
+
+export const sampleProductsArr: Product[] = [
+  {
+    id: 1,
+    name: "OnePlus 9",
+    price: 999.9,
+    discount: 10,
+  },
+  {
+    id: 2,
+    name: "OnePlus 9 Pro",
+    price: 1099.9,
+    discount: 11.5,
+  },
+];
